369062a75e89f2683bb330ac7eda7ecd15ac4bce
dimas lópez <me@dimaslz.io>
2020-06-23 12:04:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# TypeScript Node Starter
+
+My escensial config to create REST API in NodeJS with TypeScript.
+
+## ...
+...
+
+## Stack
+* MongoDB
+* Express
+* TypeScript
+
+## TODO
+- [ ] 
---
+++ src/app.ts
@@ -0,0 +1,65 @@
+import express from "express";
+import compression from "compression";  // compresses requests
+import session from "express-session";
+import bodyParser from "body-parser";
+import lusca from "lusca";
+// import mongo from "connect-mongo";
+import path from "path";
+// import mongoose from "mongoose";
+// import passport from "passport";
+// import bluebird from "bluebird";
+import { SESSION_SECRET } from "./util/secrets";
+// import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+
+// const MongoStore = mongo(session);
+
+// Controllers (route handlers)
+import * as homeController from "./controllers/home";
+
+
+// API keys and Passport configuration
+// import * as passportConfig from "./config/passport";
+
+// Create Express server
+const app = express();
+
+// Connect to MongoDB
+// const mongoUrl = MONGODB_URI;
+// mongoose.Promise = bluebird;
+
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
+//     () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+// ).catch(err => {
+//     console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+//     // process.exit();
+// });
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    // store: new MongoStore({
+    //     url: mongoUrl,
+    //     autoReconnect: true
+    // })
+}));
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+app.use(
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", homeController.index);
+
+export default app;
